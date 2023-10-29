c417c744bd303987a852bbe175f5ef96884adda5
ritiz12 <dhimanritika1212@gmail.com>
2023-10-29 17:22:46 +0530

    fifth commit

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import '../styles/calculator.css'; // Import your CSS file
 
@@ -8,63 +7,75 @@ function Calculator() {
   const [expenses, setExpenses] = useState(0);
   const [investmentType, setInvestmentType] = useState('Long Term');
   const [annualIncome, setAnnualIncome] = useState('$0 - $18200');
-  const [taxRate, setTaxRate] = useState('');
+  const [taxRate, setTaxRate] = useState('0%');
   const [capitalGainsAmount, setCapitalGainsAmount] = useState(0);
   const [longTermDiscount, setLongTermDiscount] = useState(0);
   const [netCapitalGains, setNetCapitalGains] = useState(0);
   const [taxToPay, setTaxToPay] = useState(0);
 
   useEffect(() => {
-    const capitalGainsAmount = salePrice - purchasePrice - expenses;
+    const capitalGainsAmount = parseFloat(salePrice) - parseFloat(purchasePrice) - parseFloat(expenses);
     let taxRate = '';
     let taxToPay = 0;
+    let discount = investmentType === 'Long Term' ? (capitalGainsAmount > 0 ? capitalGainsAmount * 0.5 : 0) : 0;
+    let netCapitalGains = investmentType === 'Long Term' ? (capitalGainsAmount - discount) : capitalGainsAmount;
 
-    if (investmentType === 'Long Term') {
-      let discount = 0;
-      if (capitalGainsAmount > 0) {
-        discount = capitalGainsAmount * 0.5;
-      }
+    switch (annualIncome) {
+      case '$0 - $18200':
+        taxRate = '0%';
+        break;
+      case '$18201 - $45000':
 
-      const netCapitalGains = capitalGainsAmount - discount;
+          taxToPay = (capitalGainsAmount - 18200) * 0.19;
 
-      switch (annualIncome) {
-        case '$0 - $18200':
-          taxRate = '0%';
-          break;
-        case '$18201 - $45000':
-          if (capitalGainsAmount > 18200) {
-            taxToPay = (capitalGainsAmount - 18200) * 0.19;
-          }
-          taxRate = '19% of excess over $18200';
-          break;
-        case '$45001 - $120000':
-          if (capitalGainsAmount > 1500) {
-            const excessOver45000 = capitalGainsAmount - 1500;
-            taxToPay = 5092 + (excessOver45000 * 0.325);
-          }
-          taxRate = '$5092 + 32.5% of excess over $45000';
-          break;
-        default:
-          taxRate = 'Invalid income range';
-      }
 
-      setTaxRate(taxRate);
-      setCapitalGainsAmount(capitalGainsAmount);
-      setLongTermDiscount(discount);
-      setNetCapitalGains(netCapitalGains);
-      setTaxToPay(taxToPay);
-    } else {
-      setTaxRate('Short Term Tax Rate Logic');
-      setCapitalGainsAmount(capitalGainsAmount);
-      setLongTermDiscount(0);
-      setNetCapitalGains(capitalGainsAmount);
-      setTaxToPay(0);
+        taxRate = '19% of excess over $18200';
+        break;
+      case '$45001 - $120000':
+         {
+          const excessOver45000 = capitalGainsAmount - 45000;
+          taxToPay = 5092 + (excessOver45000 * 0.325);
+
+        }
+        taxRate = '$5092 + 32.5% of excess over $45000';
+        break;
+      case '$120001 - $180000':
+        {
+          const excessOver120000 = capitalGainsAmount - 120000;
+          taxToPay = 29467 + (excessOver120000 * 0.37);
+
+        }
+        taxRate = '$29467 + 37% of excess over $120000';
+        break;
+      case '$180001+':
+        {
+          const excessOver180000 = capitalGainsAmount - 180000;
+          taxToPay = 51667 + (excessOver180000 * 0.45);
+
+        }
+        taxRate = '$51667 + 45% of excess over $180000';
+        break;
+      default:
+        taxRate = 'Invalid income range';
     }
+
+    setTaxRate(taxRate);
+    setCapitalGainsAmount(capitalGainsAmount);
+    setLongTermDiscount(discount);
+    setNetCapitalGains(netCapitalGains);
+    setTaxToPay(taxToPay);
   }, [purchasePrice, salePrice, expenses, investmentType, annualIncome]);
 
   // Function to format currency input
   const formatCurrency = (value) => {
-    return '$' + value.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    return '$' + parseFloat(value).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+  };
+
+  const handleInputChange = (event, setter) => {
+    const inputValue = event.target.value;
+    // Remove any non-numeric characters and trailing decimal point
+    const numericValue = inputValue.replace(/[^0-9.]/g, '');
+    setter(numericValue);
   };
 
   return (
@@ -79,25 +90,23 @@ function Calculator() {
             </select>
           </div>
           <div>
-            <label>Purchase Price:</label>
+            <label>Purchase Price</label>
             <input
               type="text"
               value={formatCurrency(purchasePrice)}
-              onChange={(e) => setPurchasePrice(Number(e.target.value.replace(/\$/g, '').replace(/,/g, '')))
-              }
+              onChange={(e) => handleInputChange(e, setPurchasePrice)}
             />
           </div>
           <div>
-            <label>Expenses:</label>
+            <label>Expenses</label>
             <input
               type="text"
               value={formatCurrency(expenses)}
-              onChange={(e) => setExpenses(Number(e.target.value.replace(/\$/g, '').replace(/,/g, '')))
-              }
+              onChange={(e) => handleInputChange(e, setExpenses)}
             />
           </div>
           <div>
-            <label>Annual Income:</label>
+            <label>Annual Income</label>
             <select
               value={annualIncome}
               onChange={(e) => setAnnualIncome(e.target.value)}
@@ -105,14 +114,16 @@ function Calculator() {
               <option value="$0 - $18200">$0 - $18200</option>
               <option value="$18201 - $45000">$18201 - $45000</option>
               <option value="$45001 - $120000">$45001 - $120000</option>
+              <option value="$120001 - $180000">$120001 - $180000</option>
+              <option value="$180001+">$180001+</option>
             </select>
           </div>
           <div>
-            <label>Capital Gains Amount:</label>
+            <label>Capital Gains Amount</label>
             <p id="out1">{`${formatCurrency(capitalGainsAmount)}`}</p>
           </div>
           <div>
-            <p id="out2">Net Capital Gains Amount {`${formatCurrency(netCapitalGains)}`}</p>
+            <p id="out2">Net Capital Gains Amount <br/>{`${formatCurrency(netCapitalGains)}`}</p>
           </div>
         </div>
         <div className="column">
@@ -123,35 +134,45 @@ function Calculator() {
             </select>
           </div>
           <div>
-            <label>Sale Price:</label>
+            <label>Sale Price</label>
             <input
               type="text"
               value={formatCurrency(salePrice)}
-              onChange={(e) => setSalePrice(Number(e.target.value.replace(/\$/g, '').replace(/,/g, '')))
-              }
+              onChange={(e) => handleInputChange(e, setSalePrice)}
             />
           </div>
           <div>
-            <label>Investment Type:</label>
-            <select
-              value={investmentType}
-              onChange={(e) => setInvestmentType(e.target.value)}
-            >
-              <option value="Long Term">Long Term</option>
-              <option value="Short Term">Short Term</option>
-            </select>
+            <div>
+              <label>Investment Type</label>
+            </div>
+            <div className="button-container">
+              <button
+                onClick={() => setInvestmentType('Short Term')}
+                className={investmentType === 'Short Term' ? 'active' : ''}
+              >
+                Short Term
+              </button>
+
+              <button
+                onClick={() => setInvestmentType('Long Term')}
+                className={investmentType === 'Long Term' ? 'active' : ''}
+              >
+               Long Term
+              </button>
+
+            </div>
+            <p id = "out6">  &nbsp; &nbsp; 12 &lt; month    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;     12 &gt; months</p>
           </div>
-          <div>
-            <label>Tax Rate:</label>
-            <p id="out1">{taxRate}</p>
+          <div >
+
+            <p id="out5">Tax Rate <br/>{taxRate}</p>
           </div>
           <div>
             <label>Discount for long-term gains:</label>
-            {investmentType === 'Long Term' && <p id="out1">{`${formatCurrency(longTermDiscount)}`}</p>}
+            <p id='out4'>{`${formatCurrency(longTermDiscount)}`}</p>
           </div>
           <div>
-
-            <p id="out3">Tax you need to pay {`${formatCurrency(taxToPay)}`}</p>
+            <p id="out3">Tax you need to pay  <br/>{`${formatCurrency(  taxToPay)}`}</p>
           </div>
         </div>
       </div>
